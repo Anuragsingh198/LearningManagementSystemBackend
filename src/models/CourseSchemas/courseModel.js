60dd266fc8c2abfbeae8e6d192e2dab176c1fade
Adi Jain <jainadi1717@gmail.com>
2025-07-31 15:21:51 +0530

    backend ready

--- src/models/CourseSchemas/courseModel.js
+++ src/models/CourseSchemas/courseModel.js
@@ -5,18 +5,18 @@ const courseSchema = new mongoose.Schema({
     title: { type: String, required: true },
     description: { type: String, required: true },
     instructorName: { type: String, required: true },
-    instructor: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, 
-    category: { type: String, required: true }, 
+    instructor: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    category: { type: String, required: true },
     thumbnail: { type: String, required: true },
-    thumbnailBlobName:{type:String, required:true},
+    thumbnailBlobName: { type: String, required: true },
     modules: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Module' }],
     students: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
     percentageCompleted: { type: Number, default: 0 },
     certificate: { type: String, default: '' },
     compulsory: { type: Boolean, default: 'false' },
-    courseDuration:{type:Number , default:7},
-    remark:{type:String , default:""},
-    isPublished:{type:Boolean , default:false}
+    courseDuration: { type: Number, default: 7 },
+    remark: { type: String, default: "" },
+    isPublished: { type: Boolean, default: false }
 }, { timestamps: true });
 
 const Course = mongoose.model('Course', courseSchema);
